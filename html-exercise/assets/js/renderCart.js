d161b343689ffe4d598de8764f7ab8db192e73f9
st-khanhngo <khanh.ngo@supremetech.vn>
2023-08-17 10:54:48 +0700

    basic style for cart page

--- html-exercise/assets/js/renderCart.js
+++ html-exercise/assets/js/renderCart.js
@@ -13,20 +13,26 @@ const cartList = () => {
     return `
     <ul class="cart-list">
     ${getCart().map((item) => `
-      <li>
-        ${item.discount ? `<span>${item.discount}</span>` : ""}
-        <h4>${item.name}</h4>
-        <img src =${item.imageUrl}>
-        <div>
-        <span>Price: ${item.price}</span>
-        <span>${item.discount ? `Discounted price:` + (item.price * (100 - item.discount) / 100).toFixed(2) : ''}</span>
-        <span>Quantity: ${item.quantity}</span>
-        <span>Total: ${item.total}</span>        
-      </div>
-        <div className="btn-wrapper">
-          <button class="btn btn-add" data-index=${item.id}>+</button>
+      <li class="cart-item">
+        <div class="cart row ${item.discount ? "product-discount" : ""}">
+          <div class="cart-info col col-4">
+            <h4>${item.name}</h4>
+            ${item.discount ? `<span class="badge badge-danger">-${item.discount}%</span>` : ""}
+            <img class="cart-img" src =${item.imageUrl}>
+          </div>
+          <div class="btn-wrapper cart-action col col-4">
           <button class="btn btn-reduce" data-index=${item.id}>-</button>
-          <button class="btn btn-delete" data-index=${item.id}>Delete</button>
+          <span>${item.quantity}</span>
+            <button class="btn btn-add" data-index=${item.id}>+</button>
+            <button class="btn btn-delete" data-index=${item.id}>Delete</button>
+          </div>
+          <div class="cart-price col col-4">
+            <div class="price-wrapper">
+              <span class="product-price">${item.price}</span>
+              ${item.discount ? `<span class="product-price-discount">${(item.price * (100 - item.discount) / 100).toFixed(2)}</span>` : ''}
+            </div>
+            <p>Total: <span class="product-price">${item.total}</span></p>        
+          </div>
         </div>
       </li>
     `).join('')}
@@ -37,8 +43,10 @@ const cartList = () => {
 
 export const loadCart = () => {
   const cartWrapper = document.querySelector('.section-cart .container');
-  if (cartWrapper) {
+  if (cartWrapper && getCart().length) {
     cartWrapper.innerHTML = cartList();
+  } else {
+    cartWrapper.innerHTML = `CART EMPTY`
   }
 
   const addItem = document.querySelectorAll('.btn-add');
